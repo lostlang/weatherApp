d9a504dbb4c757503b3468de520c8f5f413fda8b
lostlang <53492781+lostlang@users.noreply.github.com>
2021-08-03 03:23:57 +0300

    Add los

--- meteo_api/apps.py
+++ meteo_api/apps.py
@@ -1,6 +1,32 @@
 from django.apps import AppConfig
+from django.utils import timezone
 
+from requests import Session
 
 class MeteoApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meteo_api'
+
+
+class MeteoApi:
+    token = 'a5dc2527f3b6789c3e7d1bd23afd6583'
+    call = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric&lang=ru'
+    default_city = 'Moscow'
+    session = Session()
+    need_data = [['weather', 'description', "weather"],
+                 ['main', 'temp', "degree"],
+                 ['wind', 'speed', "wind_speed"]]
+
+    def get_url(self):
+        return self.call.format(self.default_city, self.token)
+
+    def get_data(self):
+        callback = self.session.get(self.get_url()).json()
+        out_data = dict()
+        for data in self.need_data:
+            callback_for_key1 = callback[data[0]]
+            if len(callback_for_key1) == 1:
+                callback_for_key1 = callback_for_key1[0]
+            callback_for_key2 = callback_for_key1[data[1]]
+            out_data[data[2]] = callback_for_key2
+        return out_data
--- meteo_api/views.py
+++ meteo_api/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.utils import timezone
+
+from . import apps, models
 
 # Create your views here.
 
 
 def index(request):
-    print(timezone.now())
-    return render(request, 'base.html')
+    api = apps.MeteoApi()
+    content = api.get_data()
+    log = models.Log(
+        date=timezone.now().date(),
+        time=timezone.now().time()
+    )
+    log.save()
+    return render(request, 'base.html', content)
